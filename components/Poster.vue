273dda584acf5a473ccf02cfe44fbd4d5638c61d
johnpaulfernandez <engr.jfernandez@gmail.com>
2019-01-05 12:20:51 -0800

    Fix text on mobile view, include icons and other info

--- components/Poster.vue
+++ components/Poster.vue
@@ -1,11 +1,29 @@
 <template>
   <section>
-    <div class="parallax-home mb-4">
-      <div class="parallax-overlay text-white float-left">
-        <h1 class="font-weight-bold text-uppercase">Movie Title</h1>
-        <h5
-          class="text-light"
-        >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum doloribus dolorem eligendi eum sapiente dignissimos corporis at perferendis non numquam, aliquam, hic provident libero iusto.</h5>
+    <div class="parallax-home mb-4 d-flex flex-column">
+      <div
+        class="d-flex justify-content-between align-items-center text-light mx-4 my-auto"
+        style="font-size: 6em"
+      >
+        <font-awesome-icon icon="chevron-left" class="d-none d-sm-block"/>
+        <font-awesome-icon icon="chevron-right" class="d-none d-sm-block"/>
+      </div>
+      <div class="row text-light bg-gradient">
+        <div class="col-lg-8 px-5 mb-2">
+          <h1 class="text-uppercase">Bird Box</h1>
+          <h5>1:30 | R | Drama</h5>
+          <h5
+            class="lead"
+          >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum doloribus dolorem eligendi eum sapiente dignissimos corporis at perferendis non numquam, aliquam, hic provident libero iusto.</h5>
+        </div>
+        <div class="col-lg-4 text-center">
+          <font-awesome-icon
+            id="icon"
+            icon="thumbs-down"
+            class="my-xl-4 my-sm-4 my-xs-5 mr-5 my-3 fa-flip-horizontal"
+          />
+          <font-awesome-icon id="icon" icon="thumbs-up" class="m-xl-5 m-sm-5 mx-xs-5 my-4"/>
+        </div>
       </div>
     </div>
   </section>
@@ -17,7 +35,7 @@ export default {};
 
 <style lang="scss">
 .parallax-home {
-  background-image: url('../static/cover.jpg');
+  background-image: url('../static/cover_poster.jpg');
 
   height: 95vh;
 
@@ -30,9 +48,8 @@ export default {};
   background-size: cover;
 }
 
-.parallax-overlay {
-  width: 60vw;
-  margin: 50vh 5vh;
+.bg-gradient {
   opacity: 0.9;
+  background-image: linear-gradient(to left, rgba(255, 0, 0, 0), rgb(8, 8, 8));
 }
 </style>
