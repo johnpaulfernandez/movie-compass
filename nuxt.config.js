756c053fe914c77270857773a56eae29af26d1f5
johnpaulfernandez <engr.jfernandez@gmail.com>
2019-01-07 10:39:43 -0800

    Add font-awesome plugin and css to nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,12 +24,12 @@ module.exports = {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['@fortawesome/fontawesome-svg-core/styles.css'],
 
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['~/plugins/fontawesome.js'],
 
   /*
    ** Nuxt.js modules
