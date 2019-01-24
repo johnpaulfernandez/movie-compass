d7520e0ecb29c218331ab0d89f3a869404b8d164
chartreusemongoose <lam.andrew.78@gmail.com>
2019-01-23 19:27:21 -0800

    Update CSS import, primary color, and navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,11 +17,10 @@ export default {
 
 
 <style lang="scss">
-@import "./styles/custom-bootstrap.scss";
+@import "./styles/app.scss";
 
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
-    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
